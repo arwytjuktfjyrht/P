9a026407f4ee6c89018bf1f2368684af9c3d5a45
arwytjuktfjyrht <70397165+arwytjuktfjyrht@users.noreply.github.com>
2020-11-25 15:18:07 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var ground;
+var divisionHeight = 300
+
+function setup() {
+  createCanvas(480,800);
+  ground = new Border(width/2, height-10, width, 20);
+
+
+}
+
+function draw() {
+  background(0,0,0); 
+  for (var k = 0; k <= width k = k + 80) {
+    divisions.push(new Border(k, height-divisionHeight/2, 10, divisionHeight))
+  } 
+  drawSprites();
+}
