bbbe49411878e98fd2fb40aa1f8e2bc88fd92a1c
arwytjuktfjyrht <70397165+arwytjuktfjyrht@users.noreply.github.com>
2020-11-30 20:44:42 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,13 +5,15 @@ function setup() {
   createCanvas(480,800);
   ground = new Border(width/2, height-10, width, 20);
 
+  for (var k = 0; k <= width k = k + 80) {
+    divisions.push(new Border(k, height-divisionHeight/2, 10, divisionHeight))
+  } 
 
+  
 }
 
 function draw() {
   background(0,0,0); 
-  for (var k = 0; k <= width k = k + 80) {
-    divisions.push(new Border(k, height-divisionHeight/2, 10, divisionHeight))
-  } 
+  
   drawSprites();
 }
